0a3b71d4b190f9344c250b91b118d0484f56f41e
yudylaw <yudylaw@gmail.com>
2018-12-18 21:05:46 +0800

    add tree case

--- src/test/leetcode.go
+++ src/test/leetcode.go
@@ -103,6 +103,129 @@ func intersection(nums1 []int, nums2 []int) []int {
 	return result
 }
 
+func isSubsequence(s string, t string) bool {
+	//双指针法
+	lt := len(t)
+	ls := len(s)
+	if ls > lt {
+		return false
+	}
+	i := 0
+	for _, v := range t {
+		if i < ls && int32(s[i]) == v {
+			i++
+		}
+	}
+	return i == ls
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * 中序遍历
+ */
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	list := make([]int, 0)
+	//递归实现
+	//先中序遍历左子树，然后再访问根结点，最后再中序遍历右子树
+	//非递归实现：基于栈
+	leftList := inorderTraversal(root.Left)
+	if leftList != nil {
+		list = append(list, leftList...)
+	}
+	list = append(list, root.Val)
+	rightList := inorderTraversal(root.Right)
+	if rightList != nil {
+		list = append(list, rightList...)
+	}
+	return list
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	//翻转左右子树，再替换
+	leftTree := invertTree(root.Left)
+	rightTree := invertTree(root.Right)
+
+	root.Left = rightTree
+	root.Right = leftTree
+	return root
+}
+
+//层次遍历，广度优先搜索
+func levelOrderSimple(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	//使用数组实现的队列
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+	//返回无层次的一维数组，每次添加一个节点
+	values := make([]int, 0)
+	for len(queue) > 0 {
+		node := queue[0]
+		values = append(values, node.Val)
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+		}
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+		}
+		//弹出队头
+		queue = queue[1:]
+	}
+	return values
+}
+
+//层次遍历，广度优先搜索
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+	list := make([][]int, 0)
+	//使用数组实现的队列
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		//父节点长度
+		count := len(queue)
+		values := make([]int, 0)
+		//遍历所有子节点
+		for i := 0; i < count; i++ {
+			node := queue[i]
+			values = append(values, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		//弹出上一层所有父节点
+		queue = queue[count:]
+		list = append(list, values)
+	}
+	return list
+}
+
+func reverseString(s string) string {
+	str := []byte{}
+	for i := len(s) - 1; i >= 0; i-- {
+		str = append(str, s[i])
+	}
+	return string(str[:])
+}
+
 func main() {
 	fmt.Println("hello leetcode.")
 	//nums := []int{1, 5, 7, 2, 3, 4}
@@ -118,8 +241,35 @@ func main() {
 	//flag := isPalindrome(str)
 	//fmt.Printf("flag=%v", flag)
 
-	nums1 := []int{2, 1, 2, 3, 4, 5}
-	nums2 := []int{5, 6, 1, 2, 3, 4, 5, 6}
-	nums := intersection(nums1, nums2)
-	fmt.Printf("nums=%v", nums)
+	//nums1 := []int{2, 1, 2, 3, 4, 5}
+	//nums2 := []int{5, 6, 1, 2, 3, 4, 5, 6}
+	//nums := intersection(nums1, nums2)
+	//fmt.Printf("nums=%v", nums)
+
+	//s := "abc"
+	//t := "abbc"
+	//flag := isSubsequence(s, t)
+	//fmt.Printf("flag=%v", flag)
+
+	//二叉树 binary tree
+	n8 := &TreeNode{Val: 8}
+	n7 := &TreeNode{Val: 7, Right: n8}
+	n6 := &TreeNode{Val: 6}
+	n5 := &TreeNode{Val: 5, Left: n7}
+	n4 := &TreeNode{Val: 4}
+	n3 := &TreeNode{Val: 3, Right: n6}
+	n2 := &TreeNode{Val: 2, Left: n4, Right: n5}
+	root := &TreeNode{Val: 1, Left: n2, Right: n3}
+
+	list := levelOrder(root)
+	fmt.Printf("list=%v", list)
+	//newTree := invertTree(root)
+	//list := inorderTraversal(newTree)
+	//for _, val := range list {
+	//	fmt.Printf("%d,", val)
+	//}
+
+	//str := string("ab,cdff :f")
+	//newStr := reverseString(str)
+	//fmt.Printf("%v", newStr)
 }

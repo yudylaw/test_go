9a9decb12902dbf8430a139356dc16dbc955636a
yudylaw <yudylaw@gmail.com>
2019-01-10 17:56:08 +0800

    stack

---
+++ src/test/MyStack.go
@@ -0,0 +1,56 @@
+package main
+
+import "container/list"
+
+type MyStack struct {
+	Queue *list.List
+}
+
+/** Initialize your data structure here. */
+func Constructor3() MyStack {
+	obj := MyStack{}
+	obj.Queue = list.New() //双向链表
+	return obj
+}
+
+/** Push element x onto stack. */
+func (this *MyStack) Push(x int) {
+	this.Queue.PushBack(x)
+	for i := 0; i < this.Queue.Len()-1; i++ {
+		this.Queue.MoveToBack(this.Queue.Front())
+	}
+}
+
+/** Removes the element on top of the stack and returns that element. */
+func (this *MyStack) Pop() int {
+	val := this.Queue.Front()
+	if val != nil {
+		this.Queue.Remove(val)
+	} else {
+		return -1
+	}
+	return val.Value.(int)
+}
+
+/** Get the top element. */
+func (this *MyStack) Top() int {
+	val := this.Queue.Front()
+	if val != nil {
+		return val.Value.(int)
+	}
+	return -1
+}
+
+/** Returns whether the stack is empty. */
+func (this *MyStack) Empty() bool {
+	return this.Queue.Len() == 0
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.Empty();
+ */
